ad1418291891f3b7f0fdc40e67f77cde08145c88
BlackburnCapz <thexuy@hotmail.com>
2016-05-12 11:44:17 -0600

    Update Prueba.java

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -19,6 +19,7 @@ public class Prueba {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Prueba");
                 JOptionPane.showMessageDialog(null, "Hola");
+                JOptionPane.showMessageDialog(null, "QUEONDA");
 
     }
     
